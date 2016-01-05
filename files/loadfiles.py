69eb7045e10c64d91e4f6b146177d1b32d3b6065
Lorenzo <tunedconsulting@gmail.com>
2016-01-05 01:00:59 +0100

    Minor changes

---
+++ files/loadfiles.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+import os
+import netCDF4 as nc4
+
+# ### patch for developing vm
+import sys
+import os
+sys.path.append(os.getcwd())
+sys.path.append(os.path.dirname(os.getcwd()))
+# ###########################
+
+__author__ = 'Lorenzo'
+
+_NC4_FILES = os.path.join(os.path.dirname(__file__), 'nc4')
+
+
+def return_files_paths():
+    """Walk the directory and return a list of NC4 files' paths"""
+    return [
+        os.path.join(_NC4_FILES, n)
+        for n in tuple(os.walk(_NC4_FILES))[0][2]
+        if n.endswith('.nc4')
+    ]
+
+
+def return_dataset(path=None):
+    """Pick a file path and return a DataSet object, if path is not specified
+    return the first file"""
+    if not path:
+        return nc4.Dataset(return_files_paths()[0], 'r')
+    return nc4.Dataset(path, 'r')
+
+
+
+
+
