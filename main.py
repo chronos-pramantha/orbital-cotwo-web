459d5e1b30ce4af9d14a9a649f1de158ded2bbc4
Lorenzo <tunedconsulting@gmail.com>
2016-01-07 12:26:41 +0100

    Implement data dumping from files to db

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ __author__ = 'Lorenzo'
 
 from files.loadfiles import return_dataset, return_files_paths
 from src.formatdata import create_generator_from_dataset
-
+from src.storedata import namedtuple_values_to_sql, go_execute
 
 def main():
     paths = return_files_paths()
-    print(paths)
-    dataset = return_dataset(paths[0])
+    print(paths, len(paths))
+    # try the first file
+    dataset = return_dataset(paths[1])
     luke = create_generator_from_dataset(dataset)
+    print(luke, )
 
-    for p in luke:
-        print(p)
+    # consume the generator
+    for point in luke:
         try:
+            go_execute(namedtuple_values_to_sql(point))
             next(luke)
         except StopIteration:
             break
+        except Exception:
+            continue
 
 
 if __name__ == '__main__':
